f2afb93163c764ee52a8e5afb959dcb781301cfe
KimchiKimuchi <109329238+KimchiKimuchi@users.noreply.github.com>
2022-07-15 16:01:51 +0900

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -3,11 +3,31 @@ let myImage = document.querySelector('img');
 myImage.onclick = function() {
     let mySrc = myImage.getAttribute('src');
     if(mySrc === 'images/pig.jpg') {
-      alert('ぶひっ')  
+      myImage.setAttribute('src','images/pig2.jfif');
+    } else {
       myImage.setAttribute('src','images/pig.jpg');
     }
 }
 let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h3');
+
+function setUserName() {
+  let myName = prompt('お名前は？.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'こんにちは、 ' + myName + 'さん';
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'こんにちは、 ' + storedName + 'さん';
+}
+
 myButton.onclick = function() {
-  alert('ありがとうございます！')
+  setUserName();
 }
